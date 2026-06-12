91504cfbb36af47f3b716faef074a1b7f839de64
Aluminium51 <noptanadon.p@gmail.com>
2026-06-12 10:51:25 +0700

    feat: update script.bash to specify postgres service and enhance build process

--- script.bash
+++ script.bash
@@ -1,12 +1,16 @@
 # start phase
-docker-compose up -d
+docker-compose up -d postgres
 bun run dev
 bunx prisma migrate dev --name init_database
 bunx prisma generate
+
+# all in one
+docker-compose up -d --build
 
 # view database http://localhost:5555
 bunx prisma studio
 docker-compose ps
 
 # stop phase
+docker-compose down
 docker-compose down -v
