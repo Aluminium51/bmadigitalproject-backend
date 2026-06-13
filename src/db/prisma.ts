5c4b81f2b4d531f8ba0ff16fe9896a2a96400464
Aluminium51 <noptanadon.p@gmail.com>
2026-06-13 18:53:04 +0700

    chore: update comments in prisma.ts for clarity

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -2,9 +2,9 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
-// 1. สร้าง Pool เชื่อมต่อไปที่ Postgres
+// Pool เชื่อมต่อไปที่ Postgres
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
-// 2. โยน adapter เข้าไปให้ PrismaClient ทำงาน
+// โยน adapter เข้าไปให้ PrismaClient ทำงาน
 export const prisma = new PrismaClient({ adapter });
