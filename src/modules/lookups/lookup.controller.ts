76ac2f9bf2d3cb9ca5aaef4028dcb253caae234d
Aluminium51 <noptanadon.p@gmail.com>
2026-07-16 12:20:29 +0700

    fix: correct import path for lookup service in lookup controller

--- src/modules/lookups/lookup.controller.ts
+++ src/modules/lookups/lookup.controller.ts
@@ -6,7 +6,7 @@ import {
   getDeputyGovernorsLookup,
   getDepartmentsLookup,
   getProjectStatusesLookup,
-} from "../projects/lookup.service";
+} from "../lookups/lookup.service";
 
 export const lookupController = {
   async getDivisions(c: Context) {
